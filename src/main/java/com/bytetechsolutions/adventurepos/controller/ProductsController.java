3f68b6152e1e4d85d2eeb2d68d59a953519e07f0
Jorge Morla <jorgemorlapro@gmail.com>
2024-05-26 20:41:50 -0400

    products

--- src/main/java/com/bytetechsolutions/adventurepos/controller/ProductsController.java
+++ src/main/java/com/bytetechsolutions/adventurepos/controller/ProductsController.java
@@ -1,9 +1,13 @@
 package com.bytetechsolutions.adventurepos.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import com.bytetechsolutions.adventurepos.domain.ProductRecord;
 
 
 @Controller
@@ -14,5 +18,13 @@ public class ProductsController {
     public ModelAndView handleGet(ModelAndView model) {
         model.setViewName("products/products");
         return model;
-    } 
+    }
+
+    @GetMapping("/fetchProducts")
+    public ModelAndView fetchProducts(ModelAndView model) {
+        model.addObject("products", List.of(
+            new ProductRecord("dff", "assets/img/product-2.jpg", "Apple Watch Second Gen", 220, 25, 170, "Accesorios", "Activo")));
+        model.setViewName("fragments/products :: fetchProducts");
+        return model;
+    }
 }
