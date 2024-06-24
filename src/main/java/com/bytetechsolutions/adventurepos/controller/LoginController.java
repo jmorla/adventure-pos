e443f001a89a50a619440bb8c1ac78369ee37d37
Jorge Morla <jorgemorlapro@gmail.com>
2024-06-23 23:11:02 -0400

    create product form

--- src/main/java/com/bytetechsolutions/adventurepos/controller/LoginController.java
+++ src/main/java/com/bytetechsolutions/adventurepos/controller/LoginController.java
@@ -3,15 +3,13 @@ package com.bytetechsolutions.adventurepos.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/signIn")
 public class LoginController {
 
     @GetMapping
-    public ModelAndView handleGet(ModelAndView model) {
-        model.setViewName("login");
-        return model;
+    public String handleGet() {
+        return "login";
     }
 }
